8f34c7d0c8b25ebd2638130768acfaf69e5f3f9e
kalil <wkalil416@gmail.com>
2018-12-11 12:55:29 -0500

    clean-blogz

--- Gemfile
+++ Gemfile
@@ -18,6 +18,10 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 
+gem 'frontend-generators'
+
+gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
+
 gem 'devise'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :gammers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'clean_blogs#index'
+  get  'about' 	=> 'clean_blogs#about'
+  get  'post' 	=> 'clean_blogs#post'
+  get  'contact' 	=> 'clean_blogs#contact'
+  devise_for :gammers
   resources :posts
-  root 'pages#home'
-  get '/about', to: 'pages#about'
 end
